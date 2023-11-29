bbca264846c8eabbefe36024d18d86ccbd6be64e
devinaxo <brian456251@gmail.com>
2023-11-29 03:13:25 -0300

    Initial commit

---
+++ app/src/main/java/com/example/gatodex_java/CatAdapter.java
@@ -0,0 +1,133 @@
+package com.example.gatodex_java;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.appcompat.app.AlertDialog;
+import com.bumptech.glide.Glide;
+
+public class CatAdapter extends CursorAdapter {
+    private final MainActivity mainActivity;
+
+    public CatAdapter(Context context, Cursor cursor, int flags, MainActivity mainActivity) {
+        super(context, cursor, flags);
+        this.mainActivity = mainActivity;
+    }
+
+    static class ViewHolder {
+        ImageView catImageView;
+        TextView idTextView;
+        TextView nicknameTextView;
+        TextView textViewType;
+        TextView textViewDate;
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate the item layout
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.cat_item, parent, false);
+
+        // ViewHolder pattern for efficient view reuse
+        ViewHolder viewHolder = new ViewHolder();
+        viewHolder.idTextView = view.findViewById(R.id.catIdTextView);
+        viewHolder.nicknameTextView = view.findViewById(R.id.catNicknameTextView);
+        viewHolder.textViewDate = view.findViewById(R.id.catDateTextView);
+        viewHolder.textViewType = view.findViewById(R.id.catTypeTextView);
+        viewHolder.catImageView = view.findViewById(R.id.catImageView);
+
+        view.setTag(viewHolder);
+
+        return view;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView idTextView = view.findViewById(R.id.catIdTextView);
+        TextView nicknameTextView = view.findViewById(R.id.catNicknameTextView);
+        TextView textViewType = view.findViewById(R.id.catTypeTextView);
+        TextView textViewDate = view.findViewById(R.id.catDateTextView);
+        ImageView catImageView = view.findViewById(R.id.catImageView);
+
+        long catId = cursor.getLong(cursor.getColumnIndexOrThrow("_id"));
+
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+        if (viewHolder == null) {
+            viewHolder = new ViewHolder();
+            viewHolder.catImageView = catImageView;
+
+            view.setTag(viewHolder);
+        }
+
+        // Set click listener on the ImageView to show CatDetailDialogFragment
+        viewHolder.catImageView.setOnClickListener(v -> mainActivity.showCatDetailDialog(catId));
+
+        // Extract the data from the cursor
+        int id = cursor.getInt(cursor.getColumnIndexOrThrow("_id"));
+        String nickname = cursor.getString(cursor.getColumnIndexOrThrow("nickname"));
+        String type = cursor.getString(cursor.getColumnIndexOrThrow("type"));
+        String date = cursor.getString(cursor.getColumnIndexOrThrow("date_met"));
+        String imagePath = cursor.getString(cursor.getColumnIndexOrThrow("path"));
+
+        // Extract and set data to the views
+        String idText = "ID: " + id;
+        String nicknameText = "Nickname: " + nickname;
+        String typeText = "Species: " + type;
+        String dateText = "Date met: " + date;
+
+        idTextView.setText(idText);
+        nicknameTextView.setText(nicknameText);
+        textViewType.setText(typeText);
+        textViewDate.setText(dateText);
+
+        // Load the cat image using Glide
+        Glide.with(context)
+                .load(imagePath)
+                .placeholder(R.drawable.default_cat_image) // Placeholder until the image is loaded
+                .error(R.drawable.default_cat_image)       // Error placeholder if the image fails to load, for some reason if I delete this line it doesn't load the image
+                .into(catImageView);
+
+        Button deleteCatButton = view.findViewById(R.id.deleteCatButton);
+
+        deleteCatButton.setOnClickListener(v -> {
+            // Handle the "Delete" button click
+            showDeleteConfirmationDialog(catId);
+        });
+
+    }
+    private void showDeleteConfirmationDialog(final long catId) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
+        builder.setMessage("Are you sure you want to delete this cat?");
+        builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mainActivity.getDbHelper().deleteCat(catId);
+                Cursor newCursor = mainActivity.getDbHelper().getAllCats();
+                CatAdapter.super.swapCursor(newCursor);
+                notifyDataSetChanged(); // Notify the adapter that the data set has changed
+                mainActivity.updateEmptyViewVisibility();
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.show();
+    }
+
+    @Override
+    public long getItemId(int position) {
+        Cursor cursor = getCursor();
+        // Assuming that your Cursor contains a column named "_id" as the cat ID
+        if (cursor.moveToPosition(position)) {
+            return cursor.getLong(cursor.getColumnIndexOrThrow("_id"));
+        }
+        return super.getItemId(position);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/gatodex_java/CatDbHelper.java
@@ -0,0 +1,146 @@
+package com.example.gatodex_java;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class CatDbHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "gatodex.db";
+    private static final int DATABASE_VERSION = 2;
+
+    // Table name and columns
+    private static final String TABLE_CAT = "cat";
+    static final String COLUMN_ID = "id";
+    private static final String COLUMN_IMAGE_PATH = "path";
+    private static final String COLUMN_NICKNAME = "nickname";
+    private static final String COLUMN_TYPE = "type";
+    private static final String COLUMN_PLACE_MET = "place_met";
+    private static final String COLUMN_DATE_MET = "date_met";
+
+
+    // SQL statement
+    private static final String SQL_CREATE_CAT_TABLE =
+            "CREATE TABLE " + TABLE_CAT + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    COLUMN_NICKNAME + " TEXT," +
+                    COLUMN_TYPE + " TEXT," +
+                    COLUMN_PLACE_MET + " TEXT," +
+                    COLUMN_IMAGE_PATH + " TEXT," +
+                    COLUMN_DATE_MET + " TEXT)";
+
+    public CatDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_CAT_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_CAT);
+        onCreate(db);
+    }
+
+    // Add a new cat to the database
+    public long addCat(Cat cat) {
+        SQLiteDatabase db = getWritableDatabase();
+        long newRowId = -1;
+
+        try {
+            ContentValues values = new ContentValues();
+            values.put(COLUMN_NICKNAME, cat.getNickname());
+            values.put(COLUMN_TYPE, cat.getType());
+            values.put(COLUMN_PLACE_MET, cat.getPlaceMet());
+            values.put(COLUMN_DATE_MET, cat.getDateMet());
+            values.put(COLUMN_IMAGE_PATH, cat.getImagePath());
+
+            newRowId = db.insert(TABLE_CAT, null, values);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            db.close();
+        }
+
+        return newRowId;
+    }
+
+
+    // Delete a cat from the database
+    public void deleteCat(long catId) {
+        SQLiteDatabase db = getWritableDatabase();
+        String selection = COLUMN_ID + "=?";
+        String[] selectionArgs = {String.valueOf(catId)};
+        db.delete(TABLE_CAT, selection, selectionArgs);
+    }
+
+    // Fetch all cats from the database
+    public Cursor getAllCats() {
+        SQLiteDatabase db = getReadableDatabase();
+        String[] projection = {
+                COLUMN_ID + " as _id",
+                COLUMN_NICKNAME,
+                COLUMN_TYPE,
+                COLUMN_PLACE_MET,
+                COLUMN_IMAGE_PATH,
+                COLUMN_DATE_MET};
+
+        return db.query(TABLE_CAT, projection, null, null, null, null, null);
+    }
+
+    public Cat getCatById(long catId) {
+        SQLiteDatabase db = getReadableDatabase();
+        String[] projection = {COLUMN_ID, COLUMN_NICKNAME, COLUMN_TYPE, COLUMN_PLACE_MET, COLUMN_IMAGE_PATH, COLUMN_DATE_MET};
+        String selection = COLUMN_ID + "=?";
+        String[] selectionArgs = {String.valueOf(catId)};
+
+        Cursor cursor = db.query(
+                TABLE_CAT,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+        );
+
+        Cat cat = null;
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndexOrThrow(COLUMN_ID);
+            int nicknameIndex = cursor.getColumnIndexOrThrow(COLUMN_NICKNAME);
+            int typeIndex = cursor.getColumnIndexOrThrow(COLUMN_TYPE);
+            int placeMetIndex = cursor.getColumnIndexOrThrow(COLUMN_PLACE_MET);
+            int dateMetIndex = cursor.getColumnIndexOrThrow(COLUMN_DATE_MET);
+            int pathIndex = cursor.getColumnIndexOrThrow(COLUMN_IMAGE_PATH);
+
+            long id = cursor.getLong(idIndex);
+            String nickname = cursor.getString(nicknameIndex);
+            String type = cursor.getString(typeIndex);
+            String placeMet = cursor.getString(placeMetIndex);
+            String dateMet = cursor.getString(dateMetIndex);
+            String path = cursor.getString(pathIndex);
+
+            Log.d("DB_DEBUG", "id: " + id);
+            Log.d("DB_DEBUG", "nickname: " + nickname);
+            Log.d("DB_DEBUG", "type: " + type);
+            Log.d("DB_DEBUG", "placeMet: " + placeMet);
+            Log.d("DB_DEBUG", "dateMet: " + dateMet);
+            Log.d("DB_DEBUG", "path: " + path);
+
+            cat = new Cat(id, nickname, type, placeMet, path, dateMet);
+        }
+
+        if (cursor != null) {
+            cursor.close();
+        }
+
+        return cat;
+    }
+
+}
